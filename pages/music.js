223ae9dfb6d8867068ed3834158d56a9596a3336
Chris Youngclaus <dublecy@gmail.com>
2024-09-10 19:12:15 -0400

    Added scopes to auth, added Now Playing tab

--- pages/music.js
+++ pages/music.js
@@ -1,8 +1,13 @@
 import { useSpotifyAuth } from '../src/script';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import Header from '../components/Header';
+import styled, { keyframes } from 'styled-components';
+import { useTheme } from '../components/ThemeContext';
 
 export default function Music() {
+  const { theme } = useTheme();
   const [profile, setProfile] = useState(null);
+  const [recentlyPlayed, setRecentlyPlayed] = useState(null);
 
   useSpotifyAuth();
 
@@ -34,18 +39,233 @@ export default function Music() {
     }
   };
 
+  const getRecentlyPlayedTrack = async () => {
+    if (typeof window !== 'undefined') {
+      let token = window.localStorage.getItem('spotify-token');
+      if (!token) {
+        console.log('No token found in LocalStorage. You may need to authenticate.');
+        return;
+      }
+
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=1', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        const data = await response.json();
+        console.log('Full response:', data);
+        if (response.ok) {
+          if (data.items.length > 0) {
+            console.log('Recently Played:', data);
+            setRecentlyPlayed(data.items[0]);
+          } else {
+            console.error('No recently played tracks found.');
+          }
+        } else {
+          console.error('Failed to fetch recently played:', data);
+        }
+      } catch (error) {
+        console.error('Error fetching recently played:', error);
+      }
+    }
+  };
+
+
   useEffect(() => {
     getSpotifyProfile();
-  }, []);
+    getRecentlyPlayedTrack();
+
+    if (recentlyPlayed && typeof window !== 'undefined') {
+      const songTitleElement = document.querySelector('.song-title');
+      if (songTitleElement && songTitleElement.offsetWidth > 250) { 
+        songTitleElement.classList.add('scrolled');
+      } else {
+        songTitleElement.classList.remove('scrolled');  // Remove the class if the title doesn't overflow
+      }
+    }
+  }, [recentlyPlayed]);
 
   return (
-    <div>
-      <h1>Spotify Profile</h1>
-      {profile ? (
-        <p>Welcome, {profile.display_name}</p>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <Container>
+      <Header />
+      <ContentContainer>
+        {profile ? (
+          <ProfileContainer>
+            <ProfileImage src={profile.images[1]?.url} alt="Profile Image" />
+          </ProfileContainer>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <ProfileText>dublecy</ProfileText>
+
+        {recentlyPlayed ? (
+  <RecentlyPlayedContainer>
+    <img src={recentlyPlayed.track.album.images[0].url} alt="Album Art" />
+    <h2>now bumpin</h2>
+    <RecentText>
+      <p className="song-title">{recentlyPlayed.track.name}</p>
+      <p className="artist-name">{recentlyPlayed.track.artists.map(artist => artist.name).join(', ')}</p>
+    </RecentText>
+  </RecentlyPlayedContainer>
+) : (
+  <p>Loading recently played track...</p>
+)}
+      </ContentContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: ${({ theme }) => theme.bodyBg};
+  user-select: none;
+  -webkit-user-drag: none;
+  z-index: 1;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  position: absolute;
+  width: 100vw;
+  height: calc(100vh - 60px);
+  flex-direction: column;
+  align-items: center;
+  top: 60px;
+  z-index: 2;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  height: 200px;
+  transform: translateY(-50%);
+  top: 55%;
+`;
+
+const pulse = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  10% {
+    transform: scale(1.03);
+  }
+  20% {
+    transform: scale(1);
+
+  }
+`;
+
+const ProfileImage = styled.img`
+  display: flex;
+  position: absolute;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  z-index: 5;
+  animation: ${pulse} 0.5s infinite;
+`;
+
+const ProfileText = styled.div`
+  display: flex;
+  position: absolute;
+  right: 10px;
+  transform: translateY(-10%);
+  opacity: 10%;
+  font-size: 300px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.text};
+  font-family: "DM Mono", monospace;
+  z-index: 1;
+`;
+
+const RecentlyPlayedContainer = styled.div`
+  display: flex;
+  position: absolute;
+  left: 20px;
+  top: 50px;
+  height: 80px;
+  align-items: center;
+
+  img {
+    width: 130px;
+    height: 130px;
+    border-radius: 15px;
+    animation: ${pulse} 0.5s infinite;
+    z-index: 4;
+  }
+
+  h2 {
+    position: absolute;
+    left: 54%;
+    top: -63%;
+    z-index: 4;
+    font-family: 'DM Mono', monospace;
+    font-size: 24px;
+    color: ${({ theme }) => theme.text};
+  }
+
+  p {
+    font-family: 'DM Mono', monospace;
+    font-size: 16px;
+    color: ${({ theme }) => theme.text};
+
+  }
+`;
+
+const RecentText = styled.div`
+  background-color: ${({ theme }) => theme.buttonBg};
+  padding-left: 20px;
+  transform: translateX(-30px);
+  border-radius: 30px;
+  min-width: 300px;
+  max-width: 300px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  .song-title {
+    display: inline-block;
+    white-space: nowrap;
+    text-overflow: clip;
+    margin-left: 25px;
+    position: relative;
+    text-align: left;
+  }
+
+  .scrolled {
+    animation: scrollText 10s linear forwards;
+    transform: translateX(0%);
+  }
+
+  .artist-name {
+    margin-top: 5px;
+    margin-left: 25px;
+    white-space: normal;
+    text-align: left;
+  }
+
+  @keyframes scrollText {
+    0% {
+      transform: translateX(0);
+    }
+    20% {
+      transform: translateX(0);
+    }
+    99% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(0%);
+    }
+  }
+`;
--- src/script.js
+++ src/script.js
@@ -2,8 +2,9 @@ import { useEffect } from 'react';
 
 const clientId = 'f10ec379f4d244e5ae18c8c171c04fc6'; // just a client id, nothing secret
 const redirectUri = 'http://localhost:3000/music';
-const scopes = 'user-read-private user-read-email';
+const scopes = 'user-read-private user-read-email user-read-recently-played playlist-read-private user-top-read';
 const authEndpoint = 'https://accounts.spotify.com/authorize';
+const tokenLifetime = 3600 * 1000; // 1 hour in milliseconds
 
 function getAccessTokenFromUrl() {
   if (typeof window !== 'undefined') {
@@ -22,9 +23,30 @@ function getAccessTokenFromUrl() {
 function setTokenToLocalStorage(token) {
   if (typeof window !== 'undefined' && token) {
     window.localStorage.setItem('spotify-token', token);
+    window.localStorage.setItem('spotify-token-timestamp', Date.now()); // Store the time the token was received
   } else {
     console.error('No token found in the URL');
   }
+}
+
+function getStoredToken() {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem('spotify-token');
+  }
+  return null;
+}
+
+function getTokenExpirationTime() {
+  if (typeof window !== 'undefined') {
+    const storedTimestamp = window.localStorage.getItem('spotify-token-timestamp');
+    return storedTimestamp ? parseInt(storedTimestamp) + tokenLifetime : null;
+  }
+  return null;
+}
+
+function isTokenExpired() {
+  const expirationTime = getTokenExpirationTime();
+  return !expirationTime || Date.now() > expirationTime;
 }
 
 function redirectToSpotifyAuth() {
@@ -36,10 +58,19 @@ function redirectToSpotifyAuth() {
 function checkTokenAndAuthenticate() {
   if (typeof window !== 'undefined') {
     const token = getAccessTokenFromUrl();
+    
     if (token) {
+      // Token found
       setTokenToLocalStorage(token);
-    } else if (!window.localStorage.getItem('spotify-token')) {
-      redirectToSpotifyAuth();
+    } else {
+      const storedToken = getStoredToken();
+      
+      if (!storedToken || isTokenExpired()) {
+        // No valid token or token expired
+        redirectToSpotifyAuth();
+      } else {
+        console.log('token found in LocalStorage:', storedToken);
+      }
     }
   }
 }
