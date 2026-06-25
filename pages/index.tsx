49fab7031896d56d85a75592f21eabfa0d87aa21
youngclaus <dublecy@gmail.com>
2026-06-24 21:47:22 -0700

    feat: replace home player with profile planets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
-import styled from 'styled-components';
 import React from 'react';
-import Player from '../components/Player/Player';
+import styled from 'styled-components';
 import ConstellationBackground from '../components/Index/ConstellationBackground';
-import { homeContent } from '../components/Player/playerContent';
+import PlanetProfile from '../components/Index/PlanetProfile';
 
 const Index: React.FC = () => {
   return (
     <Container>
       <ConstellationBackground />
-      <ContentContainer className="content-container">
-        <PlayerContainer>
-          <PlayerInteraction>
-            <Player content={homeContent} visible={true} />
-          </PlayerInteraction>
-        </PlayerContainer>
-        <TitleContainer>
-          <Title>chris</Title>
-          <Title>youngclaus</Title>
-        </TitleContainer>
-      </ContentContainer>
+      <PlanetProfile />
     </Container>
   );
 };
@@ -26,7 +15,6 @@ const Index: React.FC = () => {
 export default Index;
 
 const Container = styled.div`
-  display: flex;
   position: fixed;
   width: 100vw;
   height: 100vh;
@@ -36,90 +24,4 @@ const Container = styled.div`
   z-index: 0;
   user-select: none;
   -webkit-user-drag: none;
-`;
-
-const ContentContainer = styled.div`
-  pointer-events: none;
-
-  @media (max-width: 949px) {
-    display: flex;
-    position: fixed;
-    width: 100dvw;
-    height: 100dvh;
-    bottom: 0;
-    justify-content: center;
-    z-index: 5;
-  }
-
-  @media (min-width: 950px) {
-    display: flex;
-    position: fixed;
-    width: 100dvw;
-    height: 85dvh;
-    top: 50%;
-    transform: translateY(-50%);
-    align-items: center;
-    z-index: 5;
-    justify-content: space-between;
-    flex-direction: row;
-  }
-`;
-
-const TitleContainer = styled.div`
-  @media (max-width: 949px) {
-    display: none;
-    width: 0;
-    height: 0;
-  }
-
-  @media (min-width: 950px) {
-    width: 60%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    padding-left: 20px;
-  }
-`;
-
-const Title = styled.div`
-  text-align: left;
-  color: ${({ theme }) => theme.c3};
-  font-family: "DM Mono", monospace;
-  font-weight: bold;
-  opacity: 70%;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  font-size: clamp(1rem, 8vw, 9rem);
-`;
-
-const PlayerContainer = styled.div`
-  pointer-events: none;
-
-  @media (max-width: 949px) {
-    position: absolute;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    left: 50%;
-    top: 50%;
-    align-items: center;
-    transform: translate(-50%, -50%);
-    padding: 0;
-  }
-
-  @media (min-width: 950px) {
-    display: flex;
-    width: 40%;
-    height: 100%;
-    justify-content: right;
-    padding-right: 20px;
-    align-items: center;
-    z-index: 10;
-  }
-`;
-
-const PlayerInteraction = styled.div`
-  pointer-events: auto;
 `;
